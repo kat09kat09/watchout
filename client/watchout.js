8fd47c9164dc2cc9fc72dd57790d8356ce3f8abb
kat09kat09 <kat09kat09@gmail.com>
2015-12-05 15:20:56 -0500

    implemented draggable player, current score update

--- client/watchout.js
+++ client/watchout.js
@@ -5,10 +5,16 @@ var Board = function () {
     this.y = 700;
     this.numEnemies = 30;
     this.padding = 20;
-    this.score = 0;
+    this.currentScore = 0;
     this.maxScore = 0;
     this.numCollisions = 0;
 
+};
+
+var Enemy = function (id) {
+    this.x = Math.floor(Math.random () * 699);
+    this.y = Math.floor(Math.random () * 699);
+    this.id = id;
 };
 
 
@@ -17,15 +23,23 @@ var board = new Board();
 var enemies = function () {
     var arrOfEnemies = [];
   for (var i = 0; i < board.numEnemies; i++) {
-      arrOfEnemies.push(i);
+      arrOfEnemies.push(new Enemy(i));
   }
     return arrOfEnemies;
 };
 
 var allEnemies = enemies();
 
+var Player= function() {
+    this.x= 350;
+    this.y= 350;
 
- var gameBoard = d3.select('body')
+};
+
+var playerInstance = new Player();
+
+
+var gameBoard = d3.select('body')
                     .append('svg')
                     .attr('width', board.x)
                     .attr('height', board.y)
@@ -38,14 +52,14 @@ var allEnemies = enemies();
 
 
 var borderPath = gameBoard.append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("height", board.x)
-            .attr("width", board.y)
-            .style("stroke", 'black' )
-            .style("fill", "none")
-            .style("fill-opacity", 0)
-            .style("stroke-width", 5);
+                    .attr("x", 0)
+                    .attr("y", 0)
+                    .attr("height", board.x)
+                    .attr("width", board.y)
+                    .style("stroke", 'black' )
+                    .style("fill", "none")
+                    .style("fill-opacity", 0)
+                    .style("stroke-width", 5);
 
 
 var eCircles = gameBoard.selectAll('circle')
@@ -59,56 +73,75 @@ var eCircles = gameBoard.selectAll('circle')
                     .attr('cx', function () {return Math.random () * 699})
                     .attr('cy', function () {return Math.random () * 699});
 
-var player = gameBoard.append('rect')
+
+var drag = d3.behavior.drag()
+   .on("drag", function() {
+
+        //try to use call
+        d3.select(this)
+          .attr("cx", d3.event.x)
+          .attr("cy", d3.event.y);
+
+        playerInstance.x= d3.event.x;
+        playerInstance.y= d3.event.y;
+
+   });
+
+var player = gameBoard.append('circle')
                     .attr('class', 'player')
                     .style('fill', 'red')
                     .style('stroke', 'red')
-                    .attr('width', 10)
-                    .attr('height', 10)
-                    .attr('x', 350)
-                    .attr('y', 350);
+                    .attr('r', 10)
+                    .attr('cx', playerInstance.x)
+                    .attr('cy', playerInstance.y)
+                    .call(drag);
 
-var cb= function () {
-  gameBoard.selectAll("circle").transition()
-      //.each(function() {
-      //  d3.select(this)
-      //    .style('fill', 'blue')
-      //    .style('stroke', 'blue')
-      //    .style('r', 10)
-      //    .attr('cx', function () {return Math.random () * 699})
-      //    .attr('cy', function () {return Math.random () * 699});
-      //})
-     .style('fill', 'blue')
-     .style('stroke', 'blue')
-     .style('r', 10)
-     .attr('cx', function () {return Math.random () * 699})
-     .attr('cy', function () {return Math.random () * 699});
+var cb = function () {
+  gameBoard.selectAll(".enemies").transition()
+     .attr('cx', function (d, i) {
+         var enemy = allEnemies[i];
+         enemy.x= Math.floor(Math.random () * 699);
+         return enemy.x;
+     })
+     .attr('cy', function (d,i) {
+        var enemy= allEnemies[i];
+        enemy.y= Math.floor(Math.random () * 699);
+        return enemy.y;
+     });
+
+    board.currentScore += 20;
+
+    d3.selectAll('.current span')
+      .text( board.currentScore);
+
+    // span = document.getElementById("myspan");
+    // txt = document.createTextNode("your cool text");
+    // span.innerText = txt.textContent;
+
+    collisionCheck();
+
  };
 
 setInterval (cb, 1000);
 
-//var drag = d3.behavior.drag();
-var drag = d3.behavior.drag()
-    .on("drag", function(d,i) {
-        d.x += d3.event.dx
-        d.y += d3.event.dy
-        d3.select(this).attr("transform", function(d,i){
-            return "translate(" + [ d.x,d.y ] + ")"
-        })
-    });
-// d3.selectAll('.player').call(drag);
+function collisionCheck () {
+    for(var i=0 ; i< allEnemies.length; i++) {
+      console.log('enemies x', allEnemies[i].x);
+      console.log('player x',  playerInstance.x);
+      if (allEnemies[i].x === playerInstance.x && allEnemies[i].y === playerInstance.y) {
+        console.log('has a collision');
+        if (board.currentScore > board.maxScore) {
+            board.maxScore = board.currentScore;
+        }
+        board.currentScore = 0;
+        board.numCollisions++;
+      }
+    }
+    console.log(board.maxScore);
+    console.log(board.currentScore);
+    console.log(board.numCollisions);
+}
 
-//gameBoard.selectAll("circle").transition()
-    // d3.timer(function() {
 
-    //     gameBoard.selectAll("circle").transition()
-    //      .delay(1000)
-
-    //      .style('fill', 'blue')
-    //      .style('stroke', 'blue')
-    //      .style('r', 10)
-    //      .attr('cx', function () {return Math.random () * 699})
-    //      .attr('cy', function () {return Math.random () * 699});
-    // }, 1000);
 
 
